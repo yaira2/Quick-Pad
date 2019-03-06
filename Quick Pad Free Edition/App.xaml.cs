408708cdb74f35ea6b5ccb5b9e87560584ff8c9c
ATec Solution <atecsolution@outlook.com>
2019-03-05 20:19:47 -0500

    Added info to app center line

--- Quick Pad Free Edition/App.xaml.cs
+++ Quick Pad Free Edition/App.xaml.cs
@@ -34,7 +34,7 @@ namespace Quick_Pad_Free_Edition
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
-            AppCenter.Start("64a87afd-a838-4cd0-a46d-b3ea528dd53d", typeof(Analytics));
+            AppCenter.Start("64a87afd-a838-4cd0-a46d-b3ea528dd53d", typeof(Analytics)); //send analytics to app center
         }
 
         protected override void OnFileActivated(FileActivatedEventArgs args)
