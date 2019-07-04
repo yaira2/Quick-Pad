28be49b276bd74b1f037d65411ca442e7ab40e95
Yair Aichenbaum <explortec@outlook.com>
2019-07-04 18:45:04 -0400

    App can now be ran from command line or by using the 'win+r' shortcut and typing quickpad. 💻

--- Quick Pad Free Edition/App.xaml.cs
+++ Quick Pad Free Edition/App.xaml.cs
@@ -55,22 +55,13 @@ namespace Quick_Pad_Free_Edition
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
             Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
             if (rootFrame == null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    //TODO: Load state from previously suspended application
                 }
 
-                // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
@@ -78,17 +69,39 @@ namespace Quick_Pad_Free_Edition
             {
                 if (rootFrame.Content == null)
                 {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
                     rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
-
-                // Ensure the current window is active
                 Window.Current.Activate();
-
             }
         }
+
+        #region OnActivated
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            switch (args.Kind)
+            {
+                case ActivationKind.CommandLineLaunch:
+                    CommandLineActivatedEventArgs cmdLineArgs =
+                        args as CommandLineActivatedEventArgs;
+                    CommandLineActivationOperation operation = cmdLineArgs.Operation;
+                    string cmdLineString = operation.Arguments;
+                    string activationPath = operation.CurrentDirectoryPath;
+
+                    Frame rootFrame = Window.Current.Content as Frame;
+                    if (rootFrame == null)
+                    {
+                        rootFrame = new Frame();
+                        Window.Current.Content = rootFrame;
+                    }
+                    rootFrame.Navigate(typeof(MainPage));
+
+                    Window.Current.Activate();
+                    break;
+            }
+        }
+
+        #endregion
 
         /// <summary>
         /// Invoked when Navigation to a certain page fails
