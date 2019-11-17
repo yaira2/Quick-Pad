bf0622eb18b98a37821d5259615d6166cb31889c
Yair Aichenbaum <explortec@outlook.com>
2019-11-16 20:13:20 -0500

    Font settings

---
+++ QuickPad.UI/QuickPad.UI/Controls/Settings/Font.xaml.cs
@@ -0,0 +1,41 @@
+﻿using QuickPad.Mvvm;
+using QuickPad.UI.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace QuickPad.UI.Controls.Settings
+{
+    public sealed partial class Font : UserControl
+    {
+        public VisualThemeSelector VisualThemeSelector { get; } = VisualThemeSelector.Default;
+
+        public DocumentViewModel ViewModel
+        {
+            get => DataContext as DocumentViewModel;
+            set
+            {
+                if (value == null || DataContext == value) return;
+                DataContext = value;
+            }
+        }
+
+        public Font()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
