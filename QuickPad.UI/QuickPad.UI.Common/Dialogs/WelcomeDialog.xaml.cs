da759cfbdca0496ce530bb75396c620dff2fc7e0
The Sharp Ninja <ninja@thesharp.ninja>
2020-01-01 16:18:40 -0600

    Cleaned up folder structure.
    Signed-off-by: The Sharp Ninja <ninja@thesharp.ninja>

---
+++ QuickPad.UI/QuickPad.UI.Common/Dialogs/WelcomeDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using QuickPad.Mvvm.Commands;
+using QuickPad.Mvvm.Models.Theme;
+using QuickPad.Mvvm.ViewModels;
+using QuickPad.UI.Common.Theme;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace QuickPad.UI.Common.Dialogs
+{
+    public sealed partial class WelcomeDialog
+    {
+        private SettingsViewModel _settings;
+        public IVisualThemeSelector VtSelector => VisualThemeSelector.Current;
+        public QuickPadCommands Commands { get; }
+        public SettingsViewModel Settings => _settings;
+
+        public WelcomeDialog(SettingsViewModel settings)
+        {
+            _settings = settings;
+            this.InitializeComponent();
+        }
+
+        private void CmdClose_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Hide();
+        }
+    }
+}
