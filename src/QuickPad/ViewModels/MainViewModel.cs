f2f31ed2c77ecf9c38d7059ad290301ec19bde26
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2021-04-23 00:06:21 -0400

    Added more settings

--- src/QuickPad/ViewModels/MainViewModel.cs
+++ src/QuickPad/ViewModels/MainViewModel.cs
@@ -1,13 +1,54 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
+using System;
 
 namespace QuickPad.ViewModels
 {
     public class MainViewModel : ObservableObject
     {
+        private MainViewModel()
+        {
+        }
+
         public void NewFileCommand()
         {
+            Font = App.SettingsViewModel.DefaultFont;
+            FontSize = App.SettingsViewModel.DefaultFontSize;
 
+        }
+
+        private string font = App.SettingsViewModel.DefaultFont;
+
+        public string Font
+        {
+            get
+            {
+                return font;
+            }
+            set
+            {
+                if (SetProperty(ref font, value))
+                {
+                    App.SettingsViewModel.DefaultFont = value;
+                }
+            }
+        }
+
+        private Int32 fontSize = App.SettingsViewModel.DefaultFontSize;
+
+        public Int32 FontSize
+        {
+            get
+            {
+                return fontSize;
+            }
+            set
+            {
+                if (SetProperty(ref fontSize, value))
+                {
+                    App.SettingsViewModel.DefaultFontSize = value;
+                }
+            }
         }
     }
 }
--- src/QuickPad/ViewModels/SettingsViewModel.cs
+++ src/QuickPad/ViewModels/SettingsViewModel.cs
@@ -1,10 +1,106 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
 
 namespace QuickPad.ViewModels
 {
     public class SettingsViewModel : ObservableObject
     {
+        private readonly ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
 
+        public string DefaultFont
+        {
+            get => Get("Consolas");
+            set => Set(value);
+        }
+
+        public Int32 DefaultFontSize
+        {
+            get => Get(12);
+            set => Set(value);
+        }
+
+        #region ReadAndSaveSettings
+        public bool Set<TValue>(TValue value, [CallerMemberName] string propertyName = null)
+        {
+            propertyName = propertyName != null && propertyName.StartsWith("set_", StringComparison.InvariantCultureIgnoreCase)
+                ? propertyName.Substring(4)
+                : propertyName;
+
+            TValue originalValue = default;
+
+            if (localSettings.Values.ContainsKey(propertyName))
+            {
+                originalValue = Get(originalValue, propertyName);
+
+                localSettings.Values[propertyName] = value;
+                if (!base.SetProperty(ref originalValue, value, propertyName))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                localSettings.Values[propertyName] = value;
+            }
+
+            return true;
+        }
+
+        public TValue Get<TValue>(TValue defaultValue, [CallerMemberName] string propertyName = null)
+        {
+            var name = propertyName ??
+                       throw new ArgumentNullException(nameof(propertyName), "Cannot store property of unnamed.");
+
+            name = name.StartsWith("get_", StringComparison.InvariantCultureIgnoreCase)
+                ? propertyName.Substring(4)
+                : propertyName;
+
+            if (localSettings.Values.ContainsKey(name))
+            {
+                var value = localSettings.Values[name];
+
+                if (!(value is TValue tValue))
+                {
+                    if (value is IConvertible)
+                    {
+                        tValue = (TValue)Convert.ChangeType(value, typeof(TValue));
+                    }
+                    else
+                    {
+                        var valueType = value.GetType();
+                        var tryParse = typeof(TValue).GetMethod("TryParse", BindingFlags.Instance | BindingFlags.Public);
+
+                        if (tryParse == null)
+                        {
+                            return default;
+                        }
+
+                        var stringValue = value.ToString();
+                        tValue = default;
+
+                        var tryParseDelegate =
+                            (TryParseDelegate<TValue>)Delegate.CreateDelegate(valueType, tryParse, false);
+
+                        tValue = (tryParseDelegate?.Invoke(stringValue, out tValue) ?? false) ? tValue : default;
+                    }
+
+                    Set(tValue, propertyName); // Put the corrected value in settings.
+                    return tValue;
+                }
+                return tValue;
+            }
+
+            localSettings.Values[propertyName] = defaultValue;
+
+            return defaultValue;
+        }
+
+        private delegate bool TryParseDelegate<TValue>(string inValue, out TValue parsedValue);
+
+        #endregion ReadAndSaveSettings
     }
 }
