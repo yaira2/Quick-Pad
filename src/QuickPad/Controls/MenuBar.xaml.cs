bab63162236d679cf3597da5227c37676fa6749d
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2021-04-26 23:50:36 -0400

    Small stuff

--- src/QuickPad/Controls/MenuBar.xaml.cs
+++ src/QuickPad/Controls/MenuBar.xaml.cs
@@ -1,6 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -9,6 +20,11 @@ namespace QuickPad.Controls
     public sealed partial class MenuBar : UserControl
     {
         public event EventHandler NewFileInvoked;
+
+        public MenuBar()
+        {
+            this.InitializeComponent();
+        }
 
         private void NewFile_Click(object sender, RoutedEventArgs e)
         {
