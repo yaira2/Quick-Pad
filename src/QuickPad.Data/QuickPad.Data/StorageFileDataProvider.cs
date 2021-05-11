8d0b1b19c62a58858e5b2f5f4e2b44a47bd7da5b
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2021-05-11 10:21:25 -0400

    Fixed an issue where files dragged in from Files were read only

--- src/QuickPad.Data/QuickPad.Data/StorageFileDataProvider.cs
+++ src/QuickPad.Data/QuickPad.Data/StorageFileDataProvider.cs
@@ -2,6 +2,7 @@
 using QuickPad.Standard.Data;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
@@ -22,6 +23,24 @@ namespace QuickPad.Data
             return bytes;
         }
 
+        public static bool IsFileReadOnly(StorageFile file)
+        {
+            return (file.Attributes & Windows.Storage.FileAttributes.ReadOnly) != 0;
+        }
+
+        public static async Task<bool> IsFileWritable(StorageFile file)
+        {
+            try
+            {
+                using (var stream = await file.OpenStreamForWriteAsync()) { }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
         public async Task<string> SaveDataAsync(StorageFileWrapper<StorageFile> file, IWriter writer, Encoding encoding)
         {
             List<byte> allBytes = new List<byte>();
@@ -38,8 +57,19 @@ namespace QuickPad.Data
 
             var bytes = allBytes.ToArray();
 
-            // Create sample file; replace if exists.
-            await PathIO.WriteBytesAsync(file.Path, bytes);
+
+            if (IsFileReadOnly(file.File) || !await IsFileWritable(file.File))
+            {
+                // For file(s) dragged into Quick Pad, they are read-only
+                // StorageFile API won't work but can be overwritten by Win32 PathIO API
+                // In case the file is actually read-only, WriteBytesAsync will throw UnauthorizedAccessException exception
+                await PathIO.WriteBytesAsync(file.Path, bytes);
+            }
+            else // Use FileIO API to save
+            {
+                // Create file; replace if exists.
+                await FileIO.WriteBytesAsync(file.File, bytes);
+            }
 
             return $"{file.Name} was saved.";
         }
