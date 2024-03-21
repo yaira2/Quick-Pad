cb8679b7f977f0652bb4d07a18ba4d0b849f6bc5
Yair <39923744+yaira2@users.noreply.github.com>
2024-03-21 11:50:07 -0400

    Release: v6.3.3

--- src/QuickPad.UI/QuickPad.UI/Mvvm/Commands/Clipboard/CopyCommand.cs
+++ src/QuickPad.UI/QuickPad.UI/Mvvm/Commands/Clipboard/CopyCommand.cs
@@ -1,5 +1,6 @@
 ﻿using QuickPad.Mvvm.Commands;
 using QuickPad.Mvvm.ViewModels;
+using System;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
@@ -7,22 +8,26 @@ using Windows.Storage.Streams;
 
 namespace QuickPad.UI.Commands.Clipboard
 {
-    public class CopyCommand : SimpleCommand<DocumentViewModel<StorageFile, IRandomAccessStream>>, ICopyCommand<StorageFile, IRandomAccessStream>
-    {
-        public CopyCommand()
-        {
-            CanExecuteEvaluator = viewModel => viewModel.SelectedText.Length > 0;
+	public class CopyCommand : SimpleCommand<DocumentViewModel<StorageFile, IRandomAccessStream>>, ICopyCommand<StorageFile, IRandomAccessStream>
+	{
+		public CopyCommand()
+		{
+			CanExecuteEvaluator = viewModel => viewModel.SelectedText.Length > 0;
 
-            Executioner = viewModel =>
-            {
-                //send the selected text to the clipboard
-                var dataPackage = new DataPackage { RequestedOperation = DataPackageOperation.Copy };
-                dataPackage.SetText(viewModel.SelectedText);
-                Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dataPackage);
-                Windows.ApplicationModel.DataTransfer.Clipboard.Flush();
+			Executioner = viewModel =>
+			{
+				// send the selected text to the clipboard
+				try
+				{
+					var dataPackage = new DataPackage { RequestedOperation = DataPackageOperation.Copy };
+					dataPackage.SetText(viewModel.SelectedText);
+					Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dataPackage);
+					Windows.ApplicationModel.DataTransfer.Clipboard.Flush();
+				}
+				catch (Exception) { }
 
-                return Task.CompletedTask;
-            };
-        }
-    }
+				return Task.CompletedTask;
+			};
+		}
+	}
 }
