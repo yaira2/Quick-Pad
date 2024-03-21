b8abbf89866a5653e7d89984138cac9a54e079d4
Yair <39923744+yaira2@users.noreply.github.com>
2024-03-21 11:50:07 -0400

    Release: v6.3.3

--- src/QuickPad.UI/QuickPad.UI/Controls/FindAndReplaceView.xaml.cs
+++ src/QuickPad.UI/QuickPad.UI/Controls/FindAndReplaceView.xaml.cs
@@ -11,40 +11,40 @@ using Windows.UI.Xaml.Controls;
 
 namespace QuickPad.UI.Controls
 {
-    public sealed partial class FindAndReplaceView : UserControl
-    {
-        private DocumentViewModel<StorageFile, IRandomAccessStream> _viewModel;
-        public IVisualThemeSelector VtSelector => VisualThemeSelector.Current;
+	public sealed partial class FindAndReplaceView : UserControl
+	{
+		private DocumentViewModel<StorageFile, IRandomAccessStream> _viewModel;
+		public IVisualThemeSelector VtSelector => VisualThemeSelector.Current;
 
-        public WindowsSettingsViewModel Settings => App.Settings;
+		public WindowsSettingsViewModel Settings => App.Settings;
 
-        public QuickPadCommands<StorageFile, IRandomAccessStream> Commands => App.Commands;
+		public QuickPadCommands<StorageFile, IRandomAccessStream> Commands => App.Commands;
 
-        public IFindAndReplaceView<StorageFile, IRandomAccessStream> FindReplaceViewModel => ViewModel?.FindAndReplaceViewModel;
+		public IFindAndReplaceView<StorageFile, IRandomAccessStream> FindReplaceViewModel => ViewModel?.FindAndReplaceViewModel;
 
-        public DocumentViewModel<StorageFile, IRandomAccessStream> ViewModel
-        {
-            get => _viewModel;
-            set
-            {
-                if (_viewModel == value || value == null) return;
+		public DocumentViewModel<StorageFile, IRandomAccessStream> ViewModel
+		{
+			get => _viewModel;
+			set
+			{
+				if (_viewModel == value || value == null) return;
 
-                _viewModel = value;
+				_viewModel = value;
 
-                DataContext = _viewModel.FindAndReplaceViewModel;
+				DataContext = _viewModel.FindAndReplaceViewModel;
 
-                App.Controller.AddView(FindReplaceViewModel);
-            }
-        }
+				App.Controller.AddView(FindReplaceViewModel);
+			}
+		}
 
-        public FindAndReplaceView()
-        {
-            this.InitializeComponent();
+		public FindAndReplaceView()
+		{
+			this.InitializeComponent();
 
-            if (FindReplaceViewModel != null)
-            {
-                DataContext = FindReplaceViewModel;
-            }
-        }
-    }
+			if (FindReplaceViewModel != null)
+			{
+				DataContext = FindReplaceViewModel;
+			}
+		}
+	}
 }
